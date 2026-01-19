fcb08ed2968b2d5129701cb0e8df7e93a7a7c076
lucymathieson3 <lucymathieson3@gmail.com>
2026-01-19 16:06:55 +0800

    Add optional Wetherill CSV columns to Column enum

--- src/model/column.py
+++ src/model/column.py
@@ -7,3 +7,10 @@ class Column(Enum):
     U_PB_ERROR = 2
     PB_PB_VALUE = 3
     PB_PB_ERROR = 4
+    # Optional Wetherill input columns
+    #   x = 207Pb/235U
+    #   y = 206Pb/238U
+    PB207_U235_VALUE = 5
+    PB207_U235_ERROR = 6
+    PB206_U238_VALUE = 7
+    PB206_U238_ERROR = 8
